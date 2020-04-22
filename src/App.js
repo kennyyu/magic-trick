fda465ce74aac8a3c6cd41a414cfc76609eb2a84
Kenny Yu <yu.kenny124@gmail.com>
2020-04-21 21:50:29 -0700

    Magic App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,259 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Fisher-Yates Shuffle
+function shuffle(deck) {
+  let counter = deck.length;
+  while (counter) {
+    const i = Math.floor(Math.random() * counter-- );
+    const t = deck[counter];
+    deck[counter] = deck[i];
+    deck[i] = t;
+  }
 }
 
-export default App;
+function getCards() {
+  const ranks = [
+    'A',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'J',
+    'Q',
+    'K'
+  ];
+  const suits = ["♠︎", "♥︎", "♣︎", "♦︎"];
+  let cards = [];
+  for (const rank of ranks) {
+    for (const suit of suits) {
+      cards.push({
+        rank: rank,
+        suit: suit,
+      });
+    }
+  }
+  shuffle(cards);
+  return cards.slice(0, 21);
+}
+
+class MagicCardApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: getCards(),
+      stage: "select_pile_1",
+      chosen: null,
+    };
+  }
+
+  restartTrick() {
+    this.setState({
+      cards: getCards(),
+      stage: "select_pile_1",
+    });
+  }
+
+  getNextStage() {
+    switch (this.state.stage) {
+      case "select_pile_1":
+        return "select_pile_2";
+      case "select_pile_2":
+        return "select_pile_3";
+      case "select_pile_3":
+        return "reveal";
+      case "reveal":
+        return "reveal";
+    }
+  }
+
+  getInstructions() {
+    switch (this.state.stage) {
+      case "select_pile_1":
+        return "Please select the pile containing your card (3 more times)";
+      case "select_pile_2":
+        return "Please select the pile containing your card (2 more times)";
+      case "select_pile_3":
+        return "Please select the pile containing your card (last time!)";
+      case "reveal":
+        return "Your card is:";
+    }
+  }
+
+  doSelectPile(cards, pile) {
+    let newCards = cards.slice(0);
+    // Move the selected pile into the middle of the deck
+    for (let i = 0; i < 7; i++) {
+      const middleCard = 7 + i;
+      const selectedPileCard = pile * 7 + i;
+      const temp = newCards[middleCard];
+      newCards[middleCard] = newCards[selectedPileCard];
+      newCards[selectedPileCard] = temp;
+    }
+    return newCards;
+  }
+
+  doDealCards(cards) {
+    // deal out the cards to each pile, alternating piles
+    let piles = [[], [], []];
+    for (let i = 0; i < cards.length; i++) {
+      const pile = i % 3;
+      piles[pile].push(cards[i]);
+    }
+    return [].concat(piles[0]).concat(piles[1]).concat(piles[2]);
+  }
+
+  setNextState(pile) {
+    const nextStage = this.getNextStage();
+    switch (this.state.stage) {
+      case "select_pile_1":
+      case "select_pile_2": {
+        const cards = this.doDealCards(
+          this.doSelectPile(this.state.cards, pile)
+        );
+        this.setState({
+          cards: cards,
+          stage: nextStage,
+        });
+        break;
+      }
+      case "select_pile_3": {
+        // Don't deal out the cards again after the last select
+        const cards = this.doSelectPile(this.state.cards, pile);
+        this.setState({
+          chosen: cards[10],
+          stage: nextStage,
+        });
+        break;
+      }
+      case "reveal":
+        // do nothing
+        break;
+    }
+  }
+
+  render() {
+    return (
+      <Container fixed>
+        <Grid container spacing={3}>
+          <Grid item xs={3}>
+            <MagicCardPile
+              cards={this.state.cards.slice(0, 7)}
+              onClick={() => { this.setNextState(0); }}
+              stage={this.state.stage}
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <MagicCardPile
+              cards={this.state.cards.slice(7, 14)}
+              onClick={() => { this.setNextState(1); }}
+              stage={this.state.stage}
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <MagicCardPile
+              cards={this.state.cards.slice(14, 21)}
+              onClick={() => { this.setNextState(2); }}
+              stage={this.state.stage}
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <center>
+              <div className="instructions">
+                <Typography variant="h5" component="h3">
+                  {this.getInstructions()}
+                </Typography>
+                {this.state.stage === "reveal"
+                  ? <CardTile
+                      value={this.state.chosen.rank}
+                      suit={this.state.chosen.suit}
+                    />
+                  : null
+                }
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={this.restartTrick.bind(this)}
+                >
+                  Restart Magic Trick
+                </Button>
+              </div>
+            </center>
+          </Grid>
+        </Grid>
+      </Container>
+    );
+  }
+};
+
+const CardTile = (props) => {
+  if (props.suit === "♣︎" || props.suit === "♠︎") {
+    return (
+      <div className="card card-black">
+        <div className="card-tl">
+          <div className="card-value">{props.value}</div>
+          <div className="card-suit">{props.suit}</div>
+        </div>
+        <div className="card-br">
+          <div className="card-value">{props.value}</div>
+          <div className="card-suit">{props.suit}</div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="card card-red">
+        <div className="card-tl">
+          <div className="card-value">{props.value}</div>
+          <div className="card-suit">{props.suit}</div>
+        </div>
+        <div className="card-br">
+          <div className="card-value">{props.value}</div>
+          <div className="card-suit">{props.suit}</div>
+        </div>
+      </div>
+    );
+  }
+};
+
+class MagicCardPile extends React.Component {
+  render() {
+    return (
+      <Grid container spacing={3}>
+        {this.props.cards.map(item => (
+          <Grid item xs={12} key={item.rank + "-" + item.suit}>
+            <center>
+              <CardTile value={item.rank} suit={item.suit} />
+            </center>
+          </Grid>
+        ))}
+        <Grid item xs={12}>
+          <center>
+            {this.props.stage !== "reveal"
+              ? <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={this.props.onClick}
+                >
+                  My card is in this pile
+                </Button>
+              : null
+            }
+          </center>
+        </Grid>
+      </Grid>
+    )
+  }
+}
+
+export default MagicCardApp;
