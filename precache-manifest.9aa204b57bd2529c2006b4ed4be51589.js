6552d807818b46037a8501b6fc50acb566640154
Kenny Yu <yu.kenny124@gmail.com>
2020-04-21 22:09:20 -0700

    Site published at Tue Apr 21 22:09:20 PDT 2020

---
+++ precache-manifest.9aa204b57bd2529c2006b4ed4be51589.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac402d90a3c30643bf6cc1cc30d04f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5952e8640136bcd9f459",
+    "url": "/static/css/main.68c1ed1e.chunk.css"
+  },
+  {
+    "revision": "fe639b4318a106a6aafe",
+    "url": "/static/js/2.cdaf25d7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.cdaf25d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5952e8640136bcd9f459",
+    "url": "/static/js/main.4218920c.chunk.js"
+  },
+  {
+    "revision": "68d4f0f12e00bb26559e",
+    "url": "/static/js/runtime-main.7a8b5384.js"
+  }
+]);
